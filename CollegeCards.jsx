71a2bf1d6a37f6b55f52776588e0197eee5c111a
collegecards <philipperg@aol.com>
2026-03-16 21:48:00 -0400

    Add files via upload

--- CollegeCards.jsx
+++ CollegeCards.jsx
@@ -1619,17 +1619,29 @@ const PH_KEY = "YOUR_POSTHOG_KEY";
 const PH_HOST = "https://us.i.posthog.com";
 
 function initPostHog() {
-  if(window.__phLoaded) return;
+  if(window.__phLoaded || PH_KEY === "YOUR_POSTHOG_KEY") return;
   window.__phLoaded = true;
-  !function(t,e){var o,n,p,r;e.__SV||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split(".");2==o.length&&(t=t[o[0]],e=o[1]);t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}(p=t.createElement("script")).type="text/javascript",p.crossOrigin="anonymous",p.async=!0,p.src=s.api_host+"/static/array.js",(r=t.getElementsByTagName("script")[0]).parentNode.insertBefore(p,r);var u=e;for(a!==void 0?u=e[a]=[]:a="posthog",u.people=u.people||[],u.toString=function(t){var e="posthog";return"posthog"!==a&&(e+="."+a),t||(e+=" (stub)"),e},u.people.toString=function(){return u.toString(1)+" (stub)"},o="capture identify alias people.set people.set_once set_config register register_once unregister opt_out_capturing has_opted_out_capturing opt_in_capturing reset isFeatureEnabled onFeatureFlags getFeatureFlag getFeatureFlagPayload reloadFeatureFlags group updateEarlyAccessFeatureEnrollment getEarlyAccessFeatures getActiveMatchingSurveys getSurveys onSessionId".split(" "),n=0;n<o.length;n++)g(u,o[n]);e._i.push([i,s,a])},e.__SV=1)}(document,window.posthog||[]);
-  posthog.init(PH_KEY, {api_host: PH_HOST, persistence: "localStorage", autocapture: false});
+  // Load PostHog via script tag to avoid bundler issues
+  const script = document.createElement("script");
+  script.src = PH_HOST + "/static/array.js";
+  script.async = true;
+  script.onload = () => {
+    if(window.posthog) {
+      window.posthog.init(PH_KEY, {
+        api_host: PH_HOST,
+        persistence: "localStorage",
+        autocapture: false,
+      });
+    }
+  };
+  document.head.appendChild(script);
 }
 
 function track(event, props={}) {
   try {
-    if(window.posthog && PH_KEY !== "YOUR_POSTHOG_KEY") {
-      posthog.capture(event, props);
-    }
+    if(PH_KEY === "YOUR_POSTHOG_KEY") return;
+    const ph = window.posthog;
+    if(ph && ph.capture) ph.capture(event, props);
   } catch(e) {}
 }
 
